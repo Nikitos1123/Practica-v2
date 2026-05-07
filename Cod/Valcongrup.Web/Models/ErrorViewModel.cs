4f8126469ed9470f91bc6624cb9ced036c511a88
Nikitos1123 <morarunichita2@gmail.com>
2026-05-07 12:02:18 +0300

    prima data

---
+++ Cod/Valcongrup.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Valcongrup.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
